9cd8144c557d7a81f5b55e3abdf3ad2000314bc7
Marsel <marselflexov@gmail.com>
2025-03-30 13:50:02 +0500

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -14,9 +14,24 @@ public class Main {
         return Math.min(arr[n - 1], findMin(arr, n - 1));
     }
 
+    /**
+     * This method calculates the average of elements in an array.
+     * It uses a recursive approach.
+     * Time complexity: O(n), where n is the size of the array.
+     *
+     * @param arr The input array.
+     * @param n The number of elements in the array.
+     * @return The average of the array elements.
+     */
+    public static double findAverage(int[] arr, int n) {
+        if (n == 1) return arr[0];
+        return ((findAverage(arr, n - 1) * (n - 1)) + arr[n - 1]) / n;
+    }
+
     public static void main(String[] args) {
         // Example calls for each function
         int[] arr = {10, 1, 32, 3, 45};
         System.out.println("Min: " + findMin(arr, arr.length));
+        System.out.println("Average: " + findAverage(new int[]{3, 2, 4, 1}, 4));
     }
 }
